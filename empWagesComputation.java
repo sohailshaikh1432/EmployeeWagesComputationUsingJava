e6b327c1ca245acfc33d28c003bc59a31f0436ea
sohailshaikh1432 <sohailshaikh1432.ss@gmail.com>
2020-08-14 23:32:57 +0530

    Calculate Employee Wages using Switch Case

--- empWagesComputation.java
+++ empWagesComputation.java
@@ -39,7 +39,7 @@ public class EmpFullTimeWage{
 
 }
 */
-
+/*
 public class EmpPartTimeWages{
         public static void main(String[] args){
                 //Constant
@@ -60,6 +60,46 @@ public class EmpPartTimeWages{
                 }
                 EMP_WAGES=EMP_HOUR*emp_rate_per_hour;
                 System.out.print("Employee wages: "+EMP_WAGES);
+        }
+
+}
+*/
+
+public class EmpWagesSwitchCase{
+
+        //Constant
+        public static final int IS_FULL_TIME=1;
+        public static final int IS_PART_TIME=2;
+        public static final int EMP_RATE_PER_HOUR=20;
+        public static final int NUMBER_OF_WORKINGDAYS=20;
+
+        public static void main(String[] args){
+                EmpWagesSwitchCase empWagesSwitchCase=new EmpWagesSwitchCase();
+                empWagesSwitchCase.calculate20DayWage();
+        }
+        public void calculate20DayWage()
+        {
+                int emp_Hour;
+                int emp_Wages;
+                int totalEmpWages=0;
+                for ( int day=1; day<=20 ; day++ )
+                {
+                        int randomCheck=(int)(Math.random()*10)%3;
+                switch(randomCheck){
+                        case IS_FULL_TIME:
+                                emp_Hour=8;
+                                break;
+                        case IS_PART_TIME:
+                                emp_Hour=4;
+                                break;
+                        default:
+                                emp_Hour=0;
+                }
+                emp_Wages=emp_Hour*EMP_RATE_PER_HOUR;
+                totalEmpWages=totalEmpWages+emp_Wages;
+                System.out.println("Employee wages: "+emp_Wages);
+                }
+                System.out.println("Tota lWages : " +totalEmpWages);
         }
 
 }
