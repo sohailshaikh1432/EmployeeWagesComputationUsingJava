68414dadbafd448984eb696720d772d01051c40d
sohailshaikh1432 <sohailshaikh1432.ss@gmail.com>
2020-08-14 23:21:01 +0530

    Calculate Employee Full time Wages

--- empWagesComputation.java
+++ empWagesComputation.java
@@ -1,3 +1,4 @@
+/*
 public class EmpPresentOrAbsent{
         public static void main(String[] args){
                 //Constant
@@ -12,4 +13,27 @@ public class EmpPresentOrAbsent{
         }
 
 }
+*/
 
+public class EmpFullTimeWage{
+        public static void main(String[] args){
+                //Constant
+                int IS_FULL_TIME=1;
+                int EMP_HOUR=0;
+                int EMP_WAGES=0;
+
+                //Variables
+                int emp_rate_per_hour=20;
+
+                int randomCheck=(int)(Math.random()*10)%2;
+                System.out.println(randomCheck);
+                if(IS_FULL_TIME == randomCheck){
+                        EMP_HOUR=8;
+                }else{
+                        EMP_HOUR=0;
+                }
+                EMP_WAGES=EMP_HOUR*emp_rate_per_hour;
+                System.out.print("Employee wages: "+EMP_WAGES);
+        }
+
+}
