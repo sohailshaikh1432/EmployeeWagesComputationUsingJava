84ecab4963e735cbc1b2d5d320a3573585515a44
sohailshaikh1432 <sohailshaikh1432.ss@gmail.com>
2020-08-14 20:54:24 +0530

    Calculate Employee is Prsent or Absent

---
+++ empWagesComputation.java
@@ -0,0 +1,15 @@
+public class EmpPresentOrAbsent{
+        public static void main(String[] args){
+                //Constant
+                int IS_PRESENT=1;
+                int randomCheck=(int)(Math.floor(Math.random()*10)%2);
+                System.out.println(randomCheck);
+                if(IS_PRESENT == randomCheck){
+                        System.out.println("Employee is Present");
+                }else{
+                        System.out.println("Employee is Absent");
+                }
+        }
+
+}
+
