7640c494b17ba36722b0110c1f0f410f8c67097f
sohailshaikh1432 <sohailshaikh1432.ss@gmail.com>
2020-08-14 23:25:49 +0530

    Calculate Employee part time wages

--- empWagesComputation.java
+++ empWagesComputation.java
@@ -15,6 +15,7 @@ public class EmpPresentOrAbsent{
 }
 */
 
+/*
 public class EmpFullTimeWage{
         public static void main(String[] args){
                 //Constant
@@ -31,6 +32,31 @@ public class EmpFullTimeWage{
                         EMP_HOUR=8;
                 }else{
                         EMP_HOUR=0;
+                }
+                EMP_WAGES=EMP_HOUR*emp_rate_per_hour;
+                System.out.print("Employee wages: "+EMP_WAGES);
+        }
+
+}
+*/
+
+public class EmpPartTimeWages{
+        public static void main(String[] args){
+                //Constant
+                int IS_FULL_TIME=1;
+                int IS_PART_TIME=2;
+                int EMP_HOUR=0;
+                int EMP_WAGES=0;
+
+                //Variables
+                int emp_rate_per_hour=20;
+
+                int randomCheck=(int)(Math.random()*10)%3;
+                System.out.println(randomCheck);
+                if(IS_FULL_TIME == randomCheck){
+                        EMP_HOUR=8;
+                }else if(IS_PART_TIME == 2 ){
+                        EMP_HOUR=4;
                 }
                 EMP_WAGES=EMP_HOUR*emp_rate_per_hour;
                 System.out.print("Employee wages: "+EMP_WAGES);
