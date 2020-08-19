b19ca3e0eafcbd37ab1c59f0c1efb122cb98ebbd
sohailshaikh1432 <sohailshaikh1432.ss@gmail.com>
2020-08-19 12:57:19 +0530

    Compute Employee Full time wages

--- empWagesComputation.java
+++ empWagesComputation.java
@@ -1,21 +1,4 @@
-/*
-public class EmpPresentOrAbsent{
-        public static void main(String[] args){
-                //Constant
-                int IS_PRESENT=1;
-                int randomCheck=(int)(Math.floor(Math.random()*10)%2);
-                System.out.println(randomCheck);
-                if(IS_PRESENT == randomCheck){
-                        System.out.println("Employee is Present");
-                }else{
-                        System.out.println("Employee is Absent");
-                }
-        }
-
-}
-*/
-
-public class EmpFullTimeWage{
+public class empWagesComputation{
         public static void main(String[] args){
                 //Constant
                 int IS_FULL_TIME=1;
